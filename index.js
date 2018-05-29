1d0ef506b68d81db22ba74427d0baa8b60b52551
Renan Takeshi Laffin Yuhara <renan.yuhara@linx.com.br>
2018-05-29 17:18:53 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.get('/jogos', function(req,res, next) {
+    res.sendFile(path.join(__dirname+ '/views/index.html'));
+});
+
+app.get('/api/jogos', function(req,res,next) {
+    let teste = require('./repository/jogos.json');
+    res.status(200).json(teste);
+});
+
+var porta = 9098;
+app.listen(porta, function() {
+    console.log('listening on ' + porta);
+});
